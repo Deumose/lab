54c657d114110ac1d9e036e8bd2d7960c86eec7c
Deumose <demigod.qwe.qwe@gmail.com>
2019-09-23 20:47:34 +0300

    task 5

---
+++ 5/5.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	double x0, v0, t, xt;
+	const double a = -9.8;
+	std::cin >> x0 >> v0 >> t;
+	xt = x0 + v0 * t + (a * t * t) / 2;
+	std::cout << xt;
+	return 0;
+
+
+}
+
